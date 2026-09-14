29fd3399d1f07cfdce1cdb8aaa291852054212ec
Rafael Morilla <rafamorilla990@gmail.com>
2026-09-14 18:17:30 -0300

    Arreglo

--- project-root/app/Config/Routes.php
+++ project-root/app/Config/Routes.php
@@ -59,11 +59,7 @@ $routes->group('admin', ['filter' => 'adminAuth'], static function ($routes) {
     // Gestión del catálogo (libros)
     $routes->resource('libros', ['controller' => 'Admin\LibroController']);
 
-    // Administración e inventario (ejemplares físicos)
-    $routes->resource('ejemplares', ['controller' => 'Admin\EjemplarController']);
-    $routes->post('ejemplares/(:num)/marcar-perdido', 'Admin\EjemplarController::marcarPerdido/$1');
-    $routes->post('ejemplares/(:num)/marcar-danado', 'Admin\EjemplarController::marcarDanado/$1');
-    $routes->get('inventario/reportes', 'Admin\EjemplarController::reportes');
+    // Inventario / ejemplares deshabilitado en este proyecto
 
     // Socios (la ruta de historial debe ir antes del resource, que registra 'socios/(.*)' -> show)
     $routes->get('socios/(:num)/historial', 'Admin\SocioController::historial/$1');
--- project-root/app/Views/layouts/admin_layout.php
+++ project-root/app/Views/layouts/admin_layout.php
@@ -19,7 +19,6 @@
 
       <div class="admin-nav__grupo">Catálogo</div>
       <li><a href="/admin/libros">Libros y multimedia</a></li>
-      <li><a href="/admin/ejemplares">Inventario / ejemplares</a></li>
 
       <div class="admin-nav__grupo">Comunidad</div>
       <li><a href="/admin/socios">Socios</a></li>
